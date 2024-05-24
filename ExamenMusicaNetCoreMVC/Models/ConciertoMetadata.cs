c322e434b3ac7fc8a4cef6fd0d1618a46e4079e6
Asambler2 <chema508@hotmail.com>
2024-05-24 11:25:52 +0200

    Medio hecho decoradores

---
+++ ExamenMusicaNetCoreMVC/Models/ConciertoMetadata.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExamenMusicaNetCoreMVC.Models
+{
+    [ModelMetadataType(typeof(ConciertoMetadata))]
+    public partial class Concierto { }
+    public class ConciertoMetadata
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [RegularExpression("([0-9]{2})([/])([0-9]{2})([/])([0-9]{4})", ErrorMessage = "Fecha invalida")]
+        public DateTime? Fecha { get; set; }
+        [Required]
+        [MinLength(4)]
+        [MaxLength(15)]
+        public string? Genero { get; set; }
+        [Required]
+        [MinLength(4)]
+        [MaxLength(15)]
+        public string? Lugar { get; set; }
+
+    }
+}
